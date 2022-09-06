b5c6b05af5278ad5068bd2251c545100d5984f78
Murillo Souza <murillo.souza@evolucao-itanhaem.com>
2022-09-05 23:34:26 -0300

    atividade cidades

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿    Console.WriteLine("Itanhaém");
+    Console.ReadKey();
+    Console.WriteLine("Mongaguá");
+    Console.ReadKey();
+    Console.WriteLine("Peruíbe");
+    Console.ReadKey();
+    Console.WriteLine("São Vicente");
+    Console.ReadKey();
+    Console.WriteLine("Miracatu");
